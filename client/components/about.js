b816e99e064f2cc417cd8c1bf1c708a22ba8d70a
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-05-11 20:26:56 -0700

    add instagram api to retrieve feed

--- client/components/about.js
+++ client/components/about.js
@@ -1,47 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { AutomaticPrefetchPlugin } from 'webpack';
 
 const About = ({ FadeIn }) => {
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    //blank for now
+    getInstagram();
+  }, []);
+
+  const getInstagram = () => {
+    axios.get('https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=IGQVJXVmpMaURrV1A3QWUxSUFEV3hDWEdzSkhsZA3JQNWpXNTAtZA2xiVkZADNG1nVWlDaUdzZAnN4eFRfdVA5ZAkZAfTGJzZA0w2THFzaDJ2cmI5QUFydTR4X1ZAJOGtTcG50alhjRU5tMHJaUlFnWHJBbERCcWpZAN3JBbTdmalhN'
+  )
+    .then(res => {
+      let data = res.data.data
+      console.log('succesful get', data);
+      setFeed(data);
+    })
+    .catch(err => console.log('err on IG req', err));
+  }
 
   const Body = styled.div`
     display: grid;
-    grid-template-columns: auto auto auto;
+    grid-template-columns: auto auto;
   `
   const Details = styled.h1`
     color: #262626;
     text-align: center;
     font-family: 'Abel', sans-serif;
-    animation: ${FadeIn} linear 3s;
+    ${'' /* animation: ${FadeIn} linear 3s; */}
   `;
-  let items = [1, 2, 3]
-  let items2 = ['im item 1','im item 2','im item 3']
-  let items3 = [
-    {
-      name: 'Father & Husband',
-      description: 'Proud husband to my partner of 7 years'
-    },
-    {
-      name: 'Running & Worknig Out',
-      description: 'I enjoy the mental clarity and feeling of growth running and working out provide me.'
-    },
-    {
-      name: 'Photograph',
-      description: 'I love capturing photos in the moment'
-    }
-  ]
+  console.log('feed data', feed);
   return (
     <div>
       <Details>About</Details>
       <Body>
-        {items.map(item => (
-          <p style={{textAlign: "center", animation: `$FadeIn linear 3s`}}>{item}</p>
-        ))}
-        {items2.map(item => (
-          <p style={{textAlign: "center", animation: `$FadeIn linear 3s`}}>{item}</p>
-        ))}
-        {items3.map(item => (
-          <p style={{textAlign: "center", animation: `$FadeIn linear 3s`}}>{item.name}<br/>{item.description}</p>
-        ))}
+        <ul style={{listStyleType: 'none'}}>{feed.map(data => {
+         return (
+            <li>
+            <img style={{display: 'block', width: '540px', height: '540px', marginLeft: 'auto', marginRight: 'auto'}} src={data.media_url} /><br/>
+            <p style={{textAlign: 'center'}}>{data.caption}</p>
+            </li>
+         )
+        })}
+        </ul>
       </Body>
     </div>
   )
