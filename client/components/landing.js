ec14e1ad2926476e526abb89375e74f37dc9721e
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-07-11 17:51:21 -0700

    add styling and notes

--- client/components/landing.js
+++ client/components/landing.js
@@ -22,11 +22,13 @@ const Landing = ({ FadeIn }) => {
     text-align: center;
     font-family: 'Abel', sans-serif;
     animation: ${FadeIn} linear 3s;
+    color: #262626;
   `;
   const Body = styled.div`
     display: grid;
     grid-template-columns: auto;
     grid-template-rows: auto 300px 300px 300px 100px;
+    color: #262626;
   `;
   const UL = styled.ul`
     display: flex;
@@ -43,6 +45,7 @@ const Landing = ({ FadeIn }) => {
     ${'' /* width: 500px; */}
     margin: 10px;
     padding-top: 25px;
+    color: #262626;
   `;
   const Main1 = styled.div`
     display: flex;
@@ -55,12 +58,18 @@ const Landing = ({ FadeIn }) => {
     fonnt-size: 18px;
     font-family: 'Abel', sans-serif;
     animation: ${FadeIn} linear 3s;
+    color: #262626;
   `;
   const Main2 = styled.div`
     display: flex;
     flex-flow: row wrap;
     justify-content: center;
   `;
+  const Footer = styled.footer`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+  `
   return (
     <div>
     <Body>
@@ -75,18 +84,20 @@ const Landing = ({ FadeIn }) => {
         </UL>
         <Main1>
           <Details>
-            Test text
+            Div for different bodies of text describing myself
           </Details>
         </Main1>
         <Main2>
-          <Details>test text 2</Details>
+          <Details>
+            Div for maybe image carousel ?
+          </Details>
         </Main2>
-        <div>
-          <FaLinkedinIn/>
-          <FaGithub/>
-          <FaTwitter/>
-          <FaInstagram/>
-        </div>
+        <Footer>
+          <a href='https://www.linkedin.com/in/abrahamesparza/' target='_blank'><FaLinkedinIn style={{fontSize: '24px', color: '#2867B2' }}/></a>
+          <FaGithub style={{fontSize: '24px', color: '211F1F' }}/>
+          <FaTwitter style={{fontSize: '24px', color: '1DA1F2' }}/>
+          <FaInstagram style={{fontSize: '24px', color: '#5851db' }}/>
+        </Footer>
       </Body>
     </div>
   )
