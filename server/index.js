a17d0c99d59dd8c3661f998c12e31e90bb98fcec
Abraham Esparza <abrahamesparza.us@gmail.com>
2021-07-13 21:48:41 -0700

    update port

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const path = require('path');
 const cors = require('cors');
 const db = require('../database/index');
 
-const port = 5000;
+const port = 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join('public')));
 app.use(cors({
   credentials: true,
-  origin: 'localhost:5000'
+  origin: 'localhost:3000'
 }));
 
 app.get('/', (req, res) => {
